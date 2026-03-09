55edc12a78f568622baacf98fc4e4f57ec3f6c6e
Kanish-Kumar-S <kanishkumar.s2023@vitstudent.ac.in>
2026-03-09 21:34:51 +0530

    first  commit

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,29 @@
+package com.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+    App calc = new App();
+
+    @Test
+    public void testAdd(){
+        assertEquals(5, calc.add(2,3));
+    }
+
+    @Test
+    public void testSubtract(){
+        assertEquals(1, calc.subtract(3,2));
+    }
+
+    @Test
+    public void testMultiply(){
+        assertEquals(6, calc.multiply(2,3));
+    }
+
+    @Test
+    public void testDivide(){
+        assertEquals(2, calc.divide(6,3));
+    }
+}
